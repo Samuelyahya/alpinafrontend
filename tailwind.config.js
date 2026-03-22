dc557c029df37aa642bfd2c67f58796ddae35b1a
Samuel Yahya <samuelyahya25@gmail.com>
2026-03-22 18:18:11 +0700

    feat: add new SVG icons, images, and pages

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        "alpina-grey": "#828FA1",
+        "alpina-light-grey": "#A3B0C2",
+        "alpina-blue": "#362EED",
+        "alpina-orange": "#FF9A3D",
+        "alpina-gren": "#07B704",
+        "alpina-black": "#060521",
+      },
+    },
+  },
+  plugins: [],
+};
